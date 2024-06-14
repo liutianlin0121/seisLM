297697790c6c0906ad8a6ec15abebd1950b4f35e
Tianlin Liu <t.liu@unibas.ch>
2024-06-14 09:48:29 +0200

    add back old pretraining config

--- src/slurm_finetune.sh
+++ src/slurm_finetune.sh
@@ -1,13 +1,13 @@
-#!/bin/bash    
+#!/bin/bash     
 
-#SBATCH --job-name=seisLM_pretrain     #Name of your job
-#SBATCH --cpus-per-task=10    #Number of cores to reserve
-#SBATCH --mem-per-cpu=8G     #Amount of RAM/core to reserve
+#SBATCH --job-name=seisLM_finetune     #Name of your job
+#SBATCH --cpus-per-task=8    #Number of cores to reserve
+#SBATCH --mem-per-cpu=4G     #Amount of RAM/core to reserve
 #SBATCH --time=4-00:00:00      #Maximum allocated time
 #SBATCH --qos=1week         #Selected queue to allocate your job
-#SBATCH --output=seisLM_pretrain.o%j   #Path and name to the file for the STDOUT
-#SBATCH --error=seisLM_pretrain.e%j    #Path and name to the file for the STDERR
-#SBATCH --gres=gpu:4         #Number of GPUs to reserve
+#SBATCH --output=seisLM_finetune.o%j   #Path and name to the file for the STDOUT
+#SBATCH --error=seisLM_finetune.e%j    #Path and name to the file for the STDERR
+#SBATCH --gres=gpu:2         #Number of GPUs to reserve
 #SBATCH --partition=a100
 
 source ~/anaconda3/etc/profile.d/conda.sh
